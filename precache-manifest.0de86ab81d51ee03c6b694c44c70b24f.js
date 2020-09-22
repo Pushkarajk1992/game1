dda4eba52bf998e69c44c6f2c5e9ad04d05b38c0
Pushkaraj1992 <pushkaraj.kawade@gmail.com>
2020-09-22 17:55:59 +0530

    Updates

---
+++ precache-manifest.0de86ab81d51ee03c6b694c44c70b24f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24e4654332e092026b642e948c003039",
+    "url": "/game1/index.html"
+  },
+  {
+    "revision": "5f57fdb74e79c03c4fd0",
+    "url": "/game1/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "8d0af9969c73b081757f",
+    "url": "/game1/static/css/main.a78bb610.chunk.css"
+  },
+  {
+    "revision": "5f57fdb74e79c03c4fd0",
+    "url": "/game1/static/js/2.5bf55a20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/game1/static/js/2.5bf55a20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d0af9969c73b081757f",
+    "url": "/game1/static/js/main.4f98eac4.chunk.js"
+  },
+  {
+    "revision": "579bfb1e697af71897d1",
+    "url": "/game1/static/js/runtime-main.2bb6a661.js"
+  }
+]);
